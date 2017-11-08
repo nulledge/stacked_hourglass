a1e134230a6e1537a5c8f3f76753d3989c3f7d6c
I.B. Park <the.nulledge@gmail.com>
2017-11-08 16:51:24 +0900

    Fix bugs in the bottleneck
    I sort the order of layers and change the filters' size.

--- src/module/bottleneck.py
+++ src/module/bottleneck.py
@@ -15,7 +15,7 @@ def bottleneck(input, train):
         pass
     filter = Filter()
     filter.size = [1, 3, 1]
-    filter.channel = [64, 64, 256, input.get_shape()[-1]]
+    filter.channel = [128, 128, 256, input.get_shape()[-1]]
     filter.shape = [
         [filter.size[0], filter.size[0], filter.channel[-1], filter.channel[0]],
         [filter.size[1], filter.size[1], filter.channel[ 0], filter.channel[1]],
@@ -24,14 +24,12 @@ def bottleneck(input, train):
 
     assert filter.channel[-1] == filter.channel[2]
 
-    def conv(tensor, index, activation = None, filter = filter):
+    def conv(tensor, index, filter = filter):
         '''2D convolution with activation.
         
         Args:
             tensor: An input tensor in NHWC.
             index: The index of filter in 0-based index.
-            activation: The activation function after convolution. If None then
-                no activation.
             filter: The metadata of filters.
 
         Returns:
@@ -53,25 +51,24 @@ def bottleneck(input, train):
             strides = [1, 1, 1, 1],
             padding = 'SAME',
             data_format = 'NHWC') + bias
-        if activation is None:
-            return conv
-        else:
-            return activation(conv)
+        return conv
 
     skip_connection = tf.identity(input)
 
     input = tf.contrib.layers.batch_norm(inputs = input, is_training = train)
 
     with tf.variable_scope('conv_00'):
-        conv_00 = conv(tensor = input, index = 0, activation = tf.nn.relu)
+        conv_00 = conv(tensor = input, index = 0)
         conv_00 = tf.contrib.layers.batch_norm(inputs = conv_00, is_training = train)
+        conv_00 = tf.nn.relu(conv_00)
 
     with tf.variable_scope('conv_01'):
-        conv_01 = conv(tensor = conv_00, index = 1, activation = tf.nn.relu)
+        conv_01 = conv(tensor = conv_00, index = 1)
         conv_01 = tf.contrib.layers.batch_norm(inputs = conv_01, is_training = train)
+        conv_01 = tf.nn.relu(conv_01)
 
     with tf.variable_scope('conv_02'):
-        conv_02 = conv(tensor = conv_01, index = 2, activation = None)
+        conv_02 = conv(tensor = conv_01, index = 2)
 
     output = conv_02
 
--- src/test/import_module.py
+++ src/test/import_module.py
@@ -6,10 +6,11 @@ import tensorflow as tf
 
 with tf.variable_scope('bottleneck'):
     shape = [32, 64, 64, 256]
+    train = tf.constant(value = False, dtype = tf.bool)
     input = tf.get_variable(
         name = 'input',
         dtype = tf.float32,
         shape = shape)
-    assert module.bottleneck(input = input, train = False).get_shape() == shape
+    assert module.bottleneck(input = input, train = train).get_shape() == shape
 
 print('complete.')
