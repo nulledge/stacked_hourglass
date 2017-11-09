23122c59bb012e9f8ffbef13db7dc13541a54ccd
I.B. Park <the.nulledge@gmail.com>
2017-11-09 14:51:30 +0900

    Beautify hourglass and bottleneck

--- src/module/bottleneck.py
+++ src/module/bottleneck.py
@@ -8,36 +8,35 @@ import layer
 
 import tensorflow as tf
 
-def bottleneck(input, train):
+def bottleneck(input, train, name = 'bottleneck'):
     '''Bottleneck module for residual networks with BN before convolution.
 
     Args:
         input: An input tensor in NHWC. The number of channel must be 256.
         train: The flag for batch normalization.
+        name: The name of variable scope.
 
     Returns:
         The output tensor of the module.
     '''
-
     assert input.get_shape()[-1] == 256
 
-    skip_connection = tf.identity(input)
+    with tf.variable_scope(name):
 
-    input = tf.contrib.layers.batch_norm(inputs = input, is_training = train)
+        skip = tf.identity(input)
 
-    with tf.variable_scope('conv_00'):
-        conv_00 = layer.conv(tensor = input, ksize = 1, kchannel = 128)
-        conv_00 = tf.contrib.layers.batch_norm(inputs = conv_00, is_training = train)
-        conv_00 = tf.nn.relu(conv_00)
+        net = tf.contrib.layers.batch_norm(inputs = input, is_training = train)
+        with tf.variable_scope('conv_00'):
+            net = layer.conv(tensor = net, ksize = 1, kchannel = 128)
+            net = tf.contrib.layers.batch_norm(inputs = net, is_training = train)
+            net = tf.nn.relu(net)
 
-    with tf.variable_scope('conv_01'):
-        conv_01 = layer.conv(tensor = conv_00, ksize = 3, kchannel = 128)
-        conv_01 = tf.contrib.layers.batch_norm(inputs = conv_01, is_training = train)
-        conv_01 = tf.nn.relu(conv_01)
+        with tf.variable_scope('conv_01'):
+            net = layer.conv(tensor = net, ksize = 3, kchannel = 128)
+            net = tf.contrib.layers.batch_norm(inputs = net, is_training = train)
+            net = tf.nn.relu(net)
 
-    with tf.variable_scope('conv_02'):
-        conv_02 = layer.conv(tensor = conv_01, ksize = 1, kchannel = 256)
+        with tf.variable_scope('conv_02'):
+            net = layer.conv(tensor = net, ksize = 1, kchannel = 256)
 
-    output = conv_02
-
-    return tf.nn.relu(output + skip_connection)
+        return tf.nn.relu(net + skip, name = 'output')
--- src/module/hourglass.py
+++ src/module/hourglass.py
@@ -15,33 +15,28 @@ def hourglass(input, train):
     def _hourglass_internal(input, train, depth):
         assert depth >= 0
 
-        with tf.variable_scope('depth_' + str(depth)):
-            with tf.variable_scope('conv'):
-                net = bottleneck(input = input, train = train)
-            with tf.variable_scope('skip'):
-                skip = bottleneck(input = net, train = train)
-            net = tf.nn.max_pool(value = net, name = 'pool',
-                ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
+        scope = lambda name: 'depth_' + str(depth) + '/' + name
 
-            if depth == 0:
-                with tf.variable_scope('mini_conv_00'):
-                    net = bottleneck(input = net, train = train)
-                with tf.variable_scope('mini_conv_01'):
-                    net = bottleneck(input = net, train = train)
-                with tf.variable_scope('mini_conv_02'):
-                    net = bottleneck(input = net, train = train)
-            else:
-                net = _hourglass_internal(input = net,
-                    train = train, depth = depth - 1)
+        net = bottleneck(input = input, train = train, name = scope('conv'))
+        skip = bottleneck(input = net, train = train, name = scope('skip'))
+        net = tf.nn.max_pool(value = net, name = scope('pool'),
+            ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = 'SAME')
 
-            print(net)
+        if depth == 0:
+            net = bottleneck(input = net, train = train, name = scope('mini_conv_00'))
+            net = bottleneck(input = net, train = train, name = scope('mini_conv_01'))
+            net = bottleneck(input = net, train = train, name = scope('mini_conv_02'))
+        else:
+            net = _hourglass_internal(input = net,
+                train = train, depth = depth - 1)
 
-            height, width = map(lambda i: i.value*2, net.get_shape()[-3:-1])
-            net = tf.image.resize_images(
-                images = net,
-                size = [height, width],
-                method = tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-    
-            return net + skip
+        height, width = map(lambda i: i.value*2, net.get_shape()[-3:-1])
+        net = tf.image.resize_images(
+            images = net,
+            size = [height, width],
+            method = tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 
-    return _hourglass_internal(input = input, train = train, depth = 3)
+        return tf.add(net, skip, name = scope('output'))
+
+    return tf.identity(name = 'output',
+        input = _hourglass_internal(input = input, train = train, depth = 3))
