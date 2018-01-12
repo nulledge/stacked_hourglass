1ecd6908ce2e69a4f843abfe64930a423e39c601
I.B. Park <the.nulledge@gmail.com>
2018-01-12 20:40:18 +0900

    Port to jupyter notebook code with a heatmap representation

--- src/reader.py
+++ src/reader.py
@@ -5,7 +5,7 @@ class Image:
     
 class Heatmap:
     Width = Height = 64
-    Joints = 11
+    Joints = 16
 
 
 class Reader(object):
@@ -18,43 +18,48 @@ class Reader(object):
         self.__test_stream = open(test, option)
 
         self.__train_seeker = self.__test_seeker = 0
-        self.__train_size = 4502
-        self.__test_size = 501
+        self.__train_size = 19122
+        self.__test_size = 2125
 
     def __del__(self):
         self.__train_stream.close()
         self.__test_stream.close()
 
-    def __read(self, index, is_train):
-        stream = self.__train_stream if is_train else self.__test_stream
-        data = np.array(list(stream.read(Image.Width * Image.Height * Image.Channel + Heatmap.Joints * Heatmap.Width * Heatmap.Height)))
-        image = data[:Image.Width * Image.Height * Image.Channel]
-        heatmap = data[Image.Width * Image.Height * Image.Channel:]
-
-        image = image.reshape((Image.Height, Image.Width, Image.Channel))
-        heatmap = heatmap.reshape((Heatmap.Height, Heatmap.Width, Heatmap.Joints))
-
-        return image, heatmap
-
     def batch(self, size, is_train):
-        images = []
-        heatmaps = []
-
-        for index in range(size):
-
-            if is_train == True:
-                image, heatmap = self.__read(self.__train_seeker, is_train = is_train)
-                self.__train_seeker += 1
-                if self.__train_seeker >= self.__train_size:
-                    self.__train_seeker = 0
-                    self.__train_stream.seek(0)
-            else:
-                image, heatmap = self.__read(self.__test_seeker, is_train = is_train)
-                self.__test_seeker += 1
-                if self.__test_seeker >= self.__test_size:
-                    self.__test_seeker = 0
-                    self.__test_stream.seek(0)
+        if is_train == True:
+            if self.__train_seeker + size > self.__train_size:
+                raise Exception()
+            self.__train_stream.seek(Image.Width * Image.Height * Image.Channel * self.__train_seeker)
+            image = self.__train_stream.read(Image.Width * Image.Height * Image.Channel * size)
+            image = np.frombuffer(image, dtype = np.uint8, count = Image.Width * Image.Height * Image.Channel * size)
+            image = np.reshape(image, newshape = (size, Image.Height, Image.Width, Image.Channel))
             
-            images.append(image)
-            heatmaps.append(heatmap)
-        return images, heatmaps
+            self.__train_stream.seek(Image.Width * Image.Height * Image.Channel * self.__train_size
+                                     + Heatmap.Width * Heatmap.Height * Heatmap.Joints * self.__train_seeker)
+            heatmap = self.__train_stream.read(Heatmap.Width * Heatmap.Height * Heatmap.Joints * size)
+            heatmap = np.frombuffer(heatmap, dtype = np.uint8, count = Heatmap.Width * Heatmap.Height * Heatmap.Joints * size)
+            heatmap = np.reshape(heatmap, newshape = (size, Heatmap.Height, Heatmap.Width, Heatmap.Joints))
+            
+            self.__train_seeker += size
+            
+            if self.__train_seeker == self.__train_size:
+                self.__train_seeker = 0
+        else:
+            if self.__test_seeker + size > self.__test_size:
+                raise Exception()
+            self.__test_stream.seek(Image.Width * Image.Height * Image.Channel * self.__test_seeker)
+            image = self.__test_stream.read(Image.Width * Image.Height * Image.Channel * size)
+            image = np.frombuffer(image, dtype = np.uint8, count = Image.Width * Image.Height * Image.Channel * size)
+            image = np.reshape(image, newshape = (size, Image.Height, Image.Width, Image.Channel))
+            
+            self.__test_stream.seek(Image.Width * Image.Height * Image.Channel * self.__test_size
+                                     + Heatmap.Width * Heatmap.Height * Heatmap.Joints * self.__test_seeker)
+            heatmap = self.__test_stream.read(Heatmap.Width * Heatmap.Height * Heatmap.Joints * size)
+            heatmap = np.frombuffer(heatmap, dtype = np.uint8, count = Heatmap.Width * Heatmap.Height * Heatmap.Joints * size)
+            heatmap = np.reshape(heatmap, newshape = (size, Heatmap.Height, Heatmap.Width, Heatmap.Joints))
+            
+            self.__test_seeker += size
+            
+            if self.__test_seeker == self.__test_size:
+                self.__test_seeker = 0
+        return image, heatmap
